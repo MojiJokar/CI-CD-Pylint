f638bc0a1550648202c9c302e7ae63c700b5a72f
Moji <94471127+MojiJokar@users.noreply.github.com>
2025-04-16 15:18:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def hello():
-# print("   Test Pylint workflows  as  test hello")
-      print("      test hello")
-
-
-
+      # print("   Test Pylint workflows  as  test hello")
+      #print("      test hello")
+      print("test hello")
+      
 print(hello())
+#/github/workspace/main.py:2:1: E115 expected an indented block (comment)
+#/github/workspace/main.py:3:7: E111 indentation is not a multiple of 4
+#/github/workspace/main.py:3:7: E117 over-indented
+#/github/workspace/main.py:7:1: E303 too many blank lines (3)
